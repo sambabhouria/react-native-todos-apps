15a5b20e60f87d9ce92505818a9d73cbde75e522
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-11-10 15:57:29 +0100

    Adding State in react native : Using State-Hooks

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('samba');
+  const [person, setPerson] = useState({name: 'mamadou samba', age: 30});
+
+  const clickHandler = () => {
+    setName('samba bhouria');
+    setPerson({name: 'alpha mamadou', age: 25});
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>
-          Lorem ipsum <Text>dolor</Text> sit amet.
-        </Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
+      <Text>My name is {name}</Text>
+      <Text>
+        His name is {person.name} and his age is {person.age}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={clickHandler} />
       </View>
     </View>
   );
@@ -25,15 +30,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
+  buttonContainer: {
+    marginTop: 20,
+  }
 });
